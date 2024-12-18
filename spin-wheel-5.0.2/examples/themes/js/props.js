9ba19271434162cab41007ce5ae553a604eb1c25
CSC-BWA <c.schagerl@brauenwaelchli.com>
2024-12-18 17:04:42 +0100

    Upload

---
+++ spin-wheel-5.0.2/examples/themes/js/props.js
@@ -0,0 +1,195 @@
+import {AlignText} from '../../../src/constants.js';
+
+export const props = [
+
+  {
+    name: 'Roulette',
+    radius: 0.84,
+    itemLabelRadius: 0.93,
+    itemLabelRadiusMax: 0.35,
+    itemLabelRotation: 180,
+    itemLabelAlign: AlignText.left,
+    itemLabelColors: ['#fff'],
+    itemLabelBaselineOffset: -0.07,
+    itemLabelFont: 'Arial',
+    itemLabelFontSizeMax: 15,
+    itemBackgroundColors: ['#636A51', '#9AA184', '#867F7C', '#A9A19C', '#DBD5CD' ],
+    rotationSpeedMax: 500,
+    rotationResistance: -100,
+    lineWidth: 1,
+    lineColor: '#fff',
+    image: './img/bwa.svg',
+    overlayImage: './img/bwa-over.svg',
+    items: [
+      {
+        label: 'UELI',
+      },
+      {
+        label: 'DORIS',
+      },
+      {
+        label: 'MICHEL',
+      },
+      {
+        label: 'CÉLINE',
+      },
+      {
+        label: 'LUCIA',
+      },
+      {
+        label: 'CLARISSE',
+      },
+      {
+        label: 'VAINIUS',
+      },
+      {
+        label: 'GONÇALO',
+      },
+      {
+        label: 'LAURIE',
+      },
+      {
+        label: 'MARTIAL',
+      },
+      {
+        label: 'LISA',
+      },
+      {
+        label: 'PAUL',
+      },
+      {
+        label: 'JEAN',
+      },
+      {
+        label: 'CLÉLIE',
+      },
+      {
+        label: 'FILIPE',
+      },
+      {
+        label: 'JULIE',
+      },
+      {
+        label: 'ROMAIN',
+      },
+      {
+        label: 'MARGOT',
+      },
+      {
+        label: 'THIBAUD',
+      },
+      {
+        label: 'SÉVERINE',
+      },
+      {
+        label: 'FLORINE',
+      },
+      {
+        label: 'URSULA',
+      },
+      {
+        label: 'CLÉA',
+      },
+      {
+        label: 'VALENTIN',
+      },
+      {
+        label: 'CLEMENS',
+      },
+      {
+        label: 'CLAIRE',
+      },
+      {
+        label: 'JUN',
+      },
+],
+
+  },
+
+  {
+    name: 'Roulette 2',
+    radius: 0.89,
+    pointerAngle: 90,
+    itemLabelRadius: 0.92,
+    itemLabelRadiusMax: 0.37,
+    itemLabelRotation: 0,
+    itemLabelAlign: AlignText.right,
+    itemLabelColors: ['#000'],
+    itemLabelBaselineOffset: -0.06,
+    itemLabelFont: 'Rubik',
+    itemBackgroundColors: ['#fbf8c4', '#e4f1aa', '#c0d26e', '#ff7d7d'],
+    rotationSpeedMax: 700,
+    rotationResistance: -110,
+    lineWidth: 0,
+    overlayImage: './img/example-1-overlay.svg',
+    borderWidth: 0,
+    items: [
+      {
+        label: 'Japanese',
+      },
+      {
+        label: 'Fish N Chips',
+      },
+      {
+        label: 'Sandwich',
+      },
+      {
+        label: 'Sub Sandwich',
+        weight: 1.3,
+      },
+      {
+        label: 'Tacos / Mexican',
+      },
+      {
+        label: 'Noodle Box',
+      },
+      {
+        label: 'BBQ Chicken',
+      },
+      {
+        label: 'Fried Chicken',
+        weight: 1.3,
+      },
+      {
+        label: 'Indian',
+      },
+      {
+        label: 'Thai',
+      },
+      {
+        label: 'Juice Smoothie',
+      },
+      {
+        label: 'Burgers',
+        weight: 1.3,
+      },
+      {
+        label: 'Souvlaki / Kebab',
+      },
+      {
+        label: 'Italian',
+      },
+      {
+        label: 'Sushi',
+      },
+      {
+        label: 'Subways',
+        weight: 1.3,
+      },
+      {
+        label: 'Pie / Bakery',
+      },
+      {
+        label: 'Chinese',
+      },
+      {
+        label: 'Korean',
+      },
+      {
+        label: 'Pizza',
+        weight: 1.3,
+      },
+    ],
+  },
+
+];
